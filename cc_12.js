a24d162e0278ff154df18e3491234f41839b0a4b
mvivy3 <meredithyen@usf.edu>
2025-03-02 22:49:21 -0500

    Task 1 - Created Revenue Metric Card

---
+++ cc_12.js
@@ -0,0 +1,16 @@
+// Task 1: Business Dashboard - DOM Element Selection and Creation
+
+const dashboardId = document.getElementById(`dashboard`); // Selecting by ID
+const dashboardQuery = document.querySelector(`#dashboard`);// Selecting using the query selector
+
+const revenueCard = document.createElement(`div`); // Creating a new <div> element
+
+revenueCard.setAttribute("class", "metric-card"); // Metric card
+revenueCard.setAttribute("id", "revenueCard"); // Revenue Card
+
+// Populate the card with a title "Revenue" and a placeholder value $0
+revenueCard.innerHTML = ` 
+            <h3>Revenue</h3> 
+            <p>$0</p>
+        `;
+dashboardId.appendChild(revenueCard); // Append the new metric card to the dashboard
